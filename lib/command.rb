02185a3057b59c11243e6a2dd24db922843ea9ec
Solomon APPIER-SIGN <braasig@gmail.com>
2020-05-14 22:47:39 +0000

    added valid? method to check if command is a valid command change strict comparison fixed rubocop issues

--- lib/command.rb
+++ lib/command.rb
@@ -1,16 +1,17 @@
 class Command
   def deprecated?(command)
-    deprecated_commands.any? { | c | c === command }
+    deprecated_commands.any? { |c| c == command }
   end
-  
+
+  def valid?(command)
+    all.any? { |c| c == command }
+  end
+
   def deprecated_commands
-    %w'MAINTAINER'
+    %w[MAINTAINER]
   end
 
   def all
-    %w'ADD COPY ENV EXPOSE FROM LABEL STOPSIGNAL USER VOLUME WORKDIR ONBUILD ARG CMD RUN ENTRYPOINT HEALTHCHECK SHELL'
+    %w[ADD COPY ENV EXPOSE FROM LABEL STOPSIGNAL USER VOLUME WORKDIR ONBUILD ARG CMD RUN ENTRYPOINT HEALTHCHECK SHELL]
   end
 end
-
-command = Command.new
-p command.deprecated?('RUN')
