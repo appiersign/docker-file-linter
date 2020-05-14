34c486a8485c3b52c2e89a4ff981e5e2e237231c
Solomon APPIER-SIGN <braasig@gmail.com>
2020-05-14 22:49:22 +0000

    added lines to check parse instructions added lines_count to get total number of lines in docker file added dump to return all lines read added comment? to check if line is a comment

--- lib/docker_file.rb
+++ lib/docker_file.rb
@@ -1,8 +1,28 @@
 class DockerFile
 
   def initialize(file_path)
+    @lines = []
     @multiline_command = false
     @file = File.open(file_path)
+    lines
   end
 
+  def lines
+    @file.readlines.each do |line|
+      current_line = line.gsub("\n", '')
+      @lines << current_line unless current_line.empty?
+    end
+  end
+
+  def lines_count
+    @lines.length
+  end
+
+  def dump
+    @lines
+  end
+
+  def comment?(line)
+    line.split(/ /)[0] == '#'
+  end
 end
