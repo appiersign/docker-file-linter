15a32fd5896742b6e3e1c4e338d23d135e3e9295
solomon APPIER-SIGN <braasig@gmail.com>
2020-05-14 21:53:11 +0000

    added command class

---
+++ lib/command.rb
@@ -0,0 +1,16 @@
+class Command
+  def deprecated?(command)
+    deprecated_commands.any? { | c | c === command }
+  end
+  
+  def deprecated_commands
+    %w'MAINTAINER'
+  end
+
+  def all
+    %w'ADD COPY ENV EXPOSE FROM LABEL STOPSIGNAL USER VOLUME WORKDIR ONBUILD ARG CMD RUN ENTRYPOINT HEALTHCHECK SHELL'
+  end
+end
+
+command = Command.new
+p command.deprecated?('RUN')
