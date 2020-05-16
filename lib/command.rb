6ee8362088480978de09ab14d77c89099d54e17e
Solomon APPIER-SIGN <braasig@gmail.com>
2020-05-16 07:34:55 +0000

    change array to multiline

--- lib/command.rb
+++ lib/command.rb
@@ -1,5 +1,23 @@
 class Command
-  private_constant VALID_COMMANDS = %w[ADD COPY ENV EXPOSE FROM LABEL STOPSIGNAL USER VOLUME WORKDIR ONBUILD ARG CMD RUN ENTRYPOINT HEALTHCHECK SHELL].freeze
+  private_constant VALID_COMMANDS = %w[
+    ADD
+    COPY
+    ENV
+    EXPOSE
+    FROM
+    LABEL
+    STOPSIGNAL
+    USER
+    VOLUME
+    WORKDIR
+    ONBUILD
+    ARG
+    CMD
+    RUN
+    ENTRYPOINT
+    HEALTHCHECK
+    SHELL
+  ].freeze
   private_constant DEPRECATED_COMMANDS = %w[MAINTAINER].freeze
 
   def deprecated?(command)
