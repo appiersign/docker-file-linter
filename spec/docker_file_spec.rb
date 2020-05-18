b02f6c0f789b9f88b280c1fcb1204e342859b895
Solomon APPIER-SIGN <braasig@gmail.com>
2020-05-18 22:04:56 +0000

    fixed failing tests

--- spec/docker_file_spec.rb
+++ spec/docker_file_spec.rb
@@ -4,7 +4,7 @@ describe DockerFile do
   let(:docker_file) { DockerFile.new('Dockerfile') }
   describe '#lines_count' do
     it 'should return the total number of lines in the docker file' do
-      expect(docker_file.lines_count).to(eql(24))
+      expect(docker_file.lines_count).to(eql(23))
     end
   end
 
@@ -13,8 +13,6 @@ describe DockerFile do
       expect(docker_file.dump).to(eql([
                                         'FROM ubuntu:18.04',
                                         'RUN apt-get update',
-                                        'RUN echo \'debconf debconf/frontend select Noninteractive\' |
-debconf-set-selections',
                                         'RUN apt-get install -y apt-utils',
                                         'RUN apt-get install -y nano',
                                         'RUN apt-get install -y apache2 apache2-utils',
@@ -22,8 +20,7 @@ debconf-set-selections',
                                         'RUN add-apt-repository ppa:ondrej/php',
                                         'RUN apt-get update',
                                         'RUN apt-get install -y php7.4',
-                                        'RUN apt-get install -y php7.4-bcmath php7.4-bz2 php7.4-intl
-php7.4-gd php7.4-mbstring php7.4-mysql php7.4-zip',
+                                        'RUN apt-get install -y php7.4-bcmath php7.4-bz2 php7.4-intl php7.4-zip',
                                         'RUN apt-get install -y curl',
                                         'RUN apt-get install -y sudo',
                                         'RUN curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -',
